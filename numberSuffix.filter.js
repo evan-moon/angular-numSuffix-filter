d88fac85bd2cb92848f27a1be2599de8644bbd89
Evan Moon <bboydart91@gmail.com>
2017-03-28 16:32:16 +0900

    Update numberSuffix.filter.js

--- numberSuffix.filter.js
+++ numberSuffix.filter.js
@@ -1,22 +1,3 @@
-// angular.module('Utils')
-//   .filter('thousandSuffix', function () {
-//     return function (input, decimals) {
-//       var exp, rounded,
-//         suffixes = ['k', 'M', 'G', 'T', 'P', 'E'];
-//
-//       if(window.isNaN(input)) {
-//         return null;
-//       }
-//
-//       if(input < 1000) {
-//         return input;
-//       }
-//
-//       exp = Math.floor(Math.log(input) / Math.log(1000));
-//
-//       return (input / Math.pow(1000, exp)).toFixed(decimals) + suffixes[exp - 1];
-//     };
-//   });
 export function numberSuffixFilter() {
     const result = (input, decimals) => {
         const suffixes = ['k', 'M', 'G', 'T', 'P', 'E'];
